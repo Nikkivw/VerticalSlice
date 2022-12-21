ac443453a9a57ecf35b9ca8f348d8080da36ae56
Nikki van Wijngaarden <90691202+Nikkivw@users.noreply.github.com>
2022-12-21 09:09:12 +0100

    Jump works just need the others

--- Stray/Assets/Scripts/Movement/WaypointFollower.cs
+++ Stray/Assets/Scripts/Movement/WaypointFollower.cs
@@ -12,52 +12,46 @@ public class WaypointFollower : MonoBehaviour
 
     // Index of current waypoint
     private int waypointIndex = 0;
-    public GameObject Cat;
+
     // Flag to track whether the player is following waypoints or not
     public bool followingWaypoints = false;
 
     private void OnTriggerEnter(Collider other)
     {
         // If the player enters the collider, start following the waypoints
-        if (other.gameObject.tag == "Jump1" || Input.GetKeyDown(KeyCode.Space)) 
+        if (other.gameObject.tag == "Player") 
         {
-            Debug.Log("Test");
+            //Debug.Log("Test");
             followingWaypoints = true;
-            //checkwaypoint();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        if(followingWaypoints == true)
+        // If the player is not following waypoints, do nothing
+        if (!followingWaypoints)
         {
-            GetComponent<Rigidbody>().useGravity = false;
-            checkwaypoint();
+
         }
-        
-    }
-    void checkwaypoint()
-    {
+
+        // If the player has reached the end of the waypoints, allow them to move freely
+        if (waypointIndex >= waypoints.Length)
+        {
+            followingWaypoints = false;
+            return;
+        }
+
         // Get the current waypoint
         Transform targetWaypoint = waypoints[waypointIndex];
+
         // Move towards the waypoint
-        Cat.transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
         // If the player has reached the waypoint, move to the next one
         if (transform.position == targetWaypoint.position)
         {
             waypointIndex++;
         }
-        // If the player has reached the end of the waypoints, allow them to move freely
-        if (waypointIndex >= waypoints.Length)
-        {
-            followingWaypoints = false;
-            GetComponent<Rigidbody>().useGravity = true;
-            waypointIndex = 0;
-            
-        }
-        
     }
 }
